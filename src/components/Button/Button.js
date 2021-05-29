32783a78f84f4fa179d6436f86ff9c4b3407718d
ninuannvarghese <ninuann86@gmal.com>
2021-05-29 16:52:02 -0400

    Added the Funstion name

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import MatButton from "@material-ui/core/Button";
 
-const TVOButton = styled(MatButton)`
+const CustomButton = styled(MatButton)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
   border-radius: 3px;
   border: 0;
@@ -12,4 +12,4 @@ const TVOButton = styled(MatButton)`
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
 
-export default TVOButton;
+export default CustomButton;
