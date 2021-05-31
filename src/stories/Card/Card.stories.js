82773a58b245a22b2d58dafa44e992a5bde1d337
ninuannvarghese <ninuann86@gmail.com>
2021-05-31 16:56:56 -0400

    Added the template bind way

--- src/stories/Card/Card.stories.js
+++ src/stories/Card/Card.stories.js
@@ -6,14 +6,15 @@ export default {
   title: "Component/Card",
   component: CustomCard,
 };
-
-export const Default = () => (
-  <CustomCard
-    title="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies, tortor quis rhoncus mattis, nisi enim placerat leo, nec porta lectus nibh in erat. Sed mauris ipsum"
-    thumbnail="https://images.newindianexpress.com/uploads/user/imagelibrary/2019/3/7/w600X390/Take_in_the_Scenery.jpg"
-    tag="nature"
-    targetlabel="Learn More"
-    altText="Nature"
-  />
-);
+const Template = (args) => <CustomCard {...args} />;
+export const CardComponent = Template.bind({});
+CardComponent.args = {
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies, tortor quis rhoncus mattis, nisi enim placerat leo, nec porta lectus nibh in erat. Sed mauris ipsum",
+  thumbnail:
+    "https://images.newindianexpress.com/uploads/user/imagelibrary/2019/3/7/w600X390/Take_in_the_Scenery.jpg",
+  tag: "nature",
+  targetlabel: "Learn More",
+  altText: "Nature",
+};
